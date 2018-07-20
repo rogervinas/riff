8e3f25303bf2c52a48067fcb6605cd8d6a4962d9
Roger Viñas <roger.vinas@scmspain.com>
2018-07-20 13:26:56 +0200

    Initial import

---
+++ src/main/java/com/rogervinas/riff/RiffApplication.java
@@ -0,0 +1,12 @@
+package com.rogervinas.riff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiffApplication.class, args);
+	}
+}
