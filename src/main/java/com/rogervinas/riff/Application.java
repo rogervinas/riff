186e9e28405f6dfd30166e1a30026deab0cefb36
Roger Viñas <roger.vinas@scmspain.com>
2018-07-20 13:35:25 +0200

    Rename application

--- src/main/java/com/rogervinas/riff/Application.java
+++ src/main/java/com/rogervinas/riff/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RiffApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RiffApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
--- src/test/java/com/rogervinas/riff/ApplicationTest.java
+++ src/test/java/com/rogervinas/riff/ApplicationTest.java
@@ -7,10 +7,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RiffApplicationTests {
+public class ApplicationTest {
 
 	@Test
-	public void contextLoads() {
+	public void should_load_context() {
 	}
-
 }
